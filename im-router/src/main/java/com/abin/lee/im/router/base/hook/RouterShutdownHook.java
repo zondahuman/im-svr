d71b5d179dad182139a5c71b3acafcc56fe91a76
zondahuman <zondahuman@gmail.com>
2016-06-18 16:18:22 +0800

    update shutdownhook

--- im-router/src/main/java/com/abin/lee/im/router/base/hook/RouterShutdownHook.java
+++ im-router/src/main/java/com/abin/lee/im/router/base/hook/RouterShutdownHook.java
@@ -1,19 +1,19 @@
 package com.abin.lee.im.router.base.hook;
 
-import com.abin.lee.im.common.business.curator.GateWayCuratorBusiness;
+import com.abin.lee.im.common.business.curator.RouterCuratorBusiness;
 
 
 public class RouterShutdownHook extends Thread {
 
     @Override
     public void run() {
-        System.out.println("gateway shutdown starting ...");
+        System.out.println("router shutdown starting ...");
         try {
             System.out.println("删除zk的节点开始");
-            GateWayCuratorBusiness.deleteGateWayZookeeper();
+            RouterCuratorBusiness.deleteRouterZookeeper();
             System.out.println("删除zk的节点已关闭");
             System.out.println("准备关闭zkclient");
-            GateWayCuratorBusiness.closeGateWayZookeeper();
+            RouterCuratorBusiness.closeRouterZookeeper();
             System.out.println("zkclient已关闭");
         } catch (Exception e) {
             System.out.println("close zkcleint error...");
